a526efebe95a51ecd34e0b106b3f002b9a0165af
ludvigla <ludvigla@kth.se>
2022-10-26 16:54:28 +0200

    Solution to exercise 1.9: unicafe step4

--- part1/unicafe_feedback/src/App.js
+++ part1/unicafe_feedback/src/App.js
@@ -5,20 +5,29 @@ const Display = ({ text, counter }) => <div>{text + " " + counter}</div>
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 // Define Statistics component
-// Not sure if the task is to assemble all statistics into one Statistics component or 
-// if the Statistics component should be the same as Display component that I used in 
-// the previous exercise. Going for the first option...
 const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <div>
-      <Display text = "good" counter={good} />
-      <Display text = "neutral" counter={neutral} />
-      <Display text = "bad" counter={bad} />
-      <Display text = "all" counter={good + neutral + bad} />
-      <Display text = "average" counter={(good - bad)/(good + neutral + bad)} />
-      <Display text = "positive" counter={good/(good + neutral + bad) * 100 + "%"} />
-    </div>
-  )
+  if (good + neutral + bad === 0) {
+    return (
+      <div>No feedback given</div>
+    );
+  } else {
+    return (
+      <div>
+        <Display text="good" counter={good} />
+        <Display text="neutral" counter={neutral} />
+        <Display text="bad" counter={bad} />
+        <Display text="all" counter={good + neutral + bad} />
+        <Display
+          text="average"
+          counter={(good - bad) / (good + neutral + bad)}
+        />
+        <Display
+          text="positive"
+          counter={(good / (good + neutral + bad)) * 100 + "%"}
+        />
+      </div>
+    );
+  }
 };
 
 const App = () => {

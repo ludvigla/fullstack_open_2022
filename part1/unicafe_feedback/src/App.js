2e5a0da81f7ce74b5d52505538f7886619d756ab
ludvigla <ludvigla@kth.se>
2022-10-26 16:50:45 +0200

    Solution to exercise 1.8: unicafe step3

--- part1/unicafe_feedback/src/App.js
+++ part1/unicafe_feedback/src/App.js
@@ -4,6 +4,23 @@ import { useState } from 'react'
 const Display = ({ text, counter }) => <div>{text + " " + counter}</div>
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
+// Define Statistics component
+// Not sure if the task is to assemble all statistics into one Statistics component or 
+// if the Statistics component should be the same as Display component that I used in 
+// the previous exercise. Going for the first option...
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <Display text = "good" counter={good} />
+      <Display text = "neutral" counter={neutral} />
+      <Display text = "bad" counter={bad} />
+      <Display text = "all" counter={good + neutral + bad} />
+      <Display text = "average" counter={(good - bad)/(good + neutral + bad)} />
+      <Display text = "positive" counter={good/(good + neutral + bad) * 100 + "%"} />
+    </div>
+  )
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -17,12 +34,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
       <Button onClick={() => setBad(bad + 1)} text="bad"/>
       <h1>statistics</h1>
-      <Display text = "good" counter={good} />
-      <Display text = "neutral" counter={neutral} />
-      <Display text = "bad" counter={bad} />
-      <Display text = "all" counter={good + neutral + bad} />
-      <Display text = "average" counter={(good - bad)/(good + neutral + bad)} />
-      <Display text = "positive" counter={good/(good + neutral + bad) * 100 + "%"} />
+      <Statistics good={good} neutral={neutral} bad={bad} /> 
     </div>
   )
 }

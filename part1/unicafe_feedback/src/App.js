14dbaecba7d2d1589ccc2e5de3d565c86debd7e0
ludvigla <ludvigla@kth.se>
2022-10-26 16:39:44 +0200

    Solution to exercise 1.7: unicafe step2

--- part1/unicafe_feedback/src/App.js
+++ part1/unicafe_feedback/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
+// Define Display and Button components
 const Display = ({ text, counter }) => <div>{text + " " + counter}</div>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -11,13 +13,16 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} text="good"/>
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button onClick={() => setBad(bad + 1)} text="bad"/>
       <h1>statistics</h1>
       <Display text = "good" counter={good} />
       <Display text = "neutral" counter={neutral} />
       <Display text = "bad" counter={bad} />
+      <Display text = "all" counter={good + neutral + bad} />
+      <Display text = "average" counter={(good - bad)/(good + neutral + bad)} />
+      <Display text = "positive" counter={good/(good + neutral + bad) * 100 + "%"} />
     </div>
   )
 }

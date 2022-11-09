7c9de7d8e64d6d577bf01bd3cd3120ac946d35f2
ludvigla <ludvigla@kth.se>
2022-11-09 12:10:34 +0100

    Revert "Solution to exercise 2.4: Course information step9"
    This reverts commit 9b1290b29ceb099532e05e884bdd0858e1a91f5c.

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -24,62 +24,35 @@ const Course = ({ course, parts }) => (
 );
 
 const App = () => {
-  const courses = [
+  const course = 'Half Stack application development'
+  const parts = [
     {
-      name: 'Half Stack application development',
+      id: 0,
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
       id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
+      name: 'Using props to pass data',
+      exercises: 7
+    },
     {
-      name: 'Node.js',
       id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+      name: 'State of a component',
+      exercises: 14
+    },
+    {
+      id: 3,
+      name: 'Redux',
+      exercises: 11
     }
   ]
 
   return (
     <>
-      {courses.map((course) => (
-        <Course 
-          key={course.id} 
-          course={course.name} 
-          parts={course.parts} 
-        />
-      ))}
+      <Course course={course} parts={parts} />
     </>
-  );
+  )
 }
 
 export default App

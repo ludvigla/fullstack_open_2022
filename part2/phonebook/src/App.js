fd44608d70fa0fc30115fc2f19f8ec1570cc7c50
ludvigla <ludvigla@kth.se>
2022-11-09 15:11:07 +0100

    Solution to exercise 2.8: The Phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,18 +3,25 @@ import "./App.css";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' ,
+      number: '00462838238'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNote = (event) => {
     event.preventDefault();
     // prevent storing empty names
-    if (newName === '') {
+    if (newName === '' || newNumber === '') {
       return
     };
+    if(!/^[0-9-]+$/.test(newNumber)){
+      alert("Invalid characters in phone number.")
+      return
+    }
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     };
     const checkperson = persons.every(person => person.name !== newName);
     // Prevent updating phonebook if name already exists
@@ -22,10 +29,15 @@ const App = () => {
       setPersons(persons.concat(personObject)) : 
       alert(`${newName} is already added to phonebook`);
     setNewName('');
+    setNewNumber('');
   }
 
   const handleNoteChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -36,13 +48,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNoteChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul className="no-bullets">
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name + "\t" + person.number}</li>
         ))}
       </ul>
     </div>

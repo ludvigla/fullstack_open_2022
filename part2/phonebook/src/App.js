1939a2dbcb0a66c9f677ed1c4edeec9a840e10b1
ludvigla <ludvigla@kth.se>
2022-11-09 15:50:40 +0100

    Solution to exercise 2.9: The Phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,25 +3,30 @@ import "./App.css";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' ,
-      number: '00462838238'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 , important: true },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 , important: true  },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 , important: true  },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 , important: true  }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+ 
   const addNote = (event) => {
     event.preventDefault();
     // prevent storing empty names
     if (newName === '' || newNumber === '') {
       return
     };
+    // prevent the usage characters other than numbers of hyphens
     if(!/^[0-9-]+$/.test(newNumber)){
       alert("Invalid characters in phone number.")
       return
     }
     const personObject = {
+      id: persons.length + 1,
       name: newName,
-      number: newNumber
+      number: newNumber,
+      important: true
     };
     const checkperson = persons.every(person => person.name !== newName);
     // Prevent updating phonebook if name already exists
@@ -40,9 +45,26 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchChange = (event) => {
+    const copy = persons.map((person) => {
+        person.name.toLowerCase().includes(event.target.value.toLowerCase())
+          ? (person.important = true)
+          : (person.important = false)
+        return person
+      }
+    );
+    setPersons(copy);
+    console.log(persons);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input onChange={handleSearchChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNote}>
         <div>
           name: <input value={newName} onChange={handleNoteChange} />
@@ -56,9 +78,14 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul className="no-bullets">
-        {persons.map((person) => (
-          <li key={person.name}>{person.name + "\t" + person.number}</li>
-        ))}
+        {persons.map((person) => {
+          console.log(person);
+          return (
+            person.important ? (
+              <li key={person.id}>{person.name + "\t" + person.number}</li>
+            ) : null
+          )
+        })}
       </ul>
     </div>
   );

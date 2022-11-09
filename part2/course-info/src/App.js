43bd74d61dcc1fadcacb03a3ba3bf6cc5056fbb4
ludvigla <ludvigla@kth.se>
2022-11-09 12:18:38 +0100

    Solution to exercise 2.5: separate module

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -1,29 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {parts.map(part => <Part key={part.id} part={part} />)}  
-  </>
-
-const Total = ({ parts }) => 
-<>
-  <strong>
-    Total of {parts.map(part => part.exercises).reduce((a, b) => a + b, 0)} execises 
-  </strong>
-</>
-
-const Course = ({ course, parts }) => (
-  <>
-    <Header course={course} />
-    <Content parts={parts} />
-    <Total parts={parts} />
-  </>
-);
+import Course from './components/Course';
 
 const App = () => {
   const courses = [

d81d6e8a4638c6974394b579fd04f03dd012db51
ludvigla <ludvigla@kth.se>
2022-11-21 16:55:08 +0100

    Solution to exercise 2.13*: Data for countries step3

--- part2/countries/src/components/ListCountries.js
+++ part2/countries/src/components/ListCountries.js
@@ -1,10 +1,31 @@
-const ListCountries = ({ countries }) => 
-  <>
-    <ul className="no-bullets">
-      {countries.map((country, index) => (
-        <li key={index}>{country.name.common}</li>
-      ))}
-    </ul>
-  </>
+import { useState, useEffect } from 'react';
+import CountryBasics from "./CountryBasics";
+
+const ListCountries = ({ countries }) => {
+
+    const [ showCountry, setShowCountry ] = useState([]);
+
+    useEffect(() => {
+        setShowCountry(countries);
+    }, [countries]);
+
+  return (
+    <>
+        <ul className="no-bullets">
+        {showCountry.map((country, index) => (
+            <li key={index}>
+                {country.name.common}
+                <input key={index} type="button" value="show" onClick={() => {
+                    const copy = [...showCountry];
+                    copy[index].show = !copy[index].show;
+                    setShowCountry(copy);
+                }}/>
+                {country.show ? <CountryBasics country={country} /> : null}
+            </li>
+        ))}
+        </ul>
+    </>
+  )
+};
 
 export default ListCountries;

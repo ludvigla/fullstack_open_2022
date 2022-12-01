19d61109cf58341d97c0d18c52644c3fa01c368c
ludvigla <ludvigla@kth.se>
2022-12-01 15:16:20 +0100

    Solution to exercise 4.4: Blog list, step4

--- part4/blog-list-app/tests/helper.test.js
+++ part4/blog-list-app/tests/helper.test.js
@@ -6,3 +6,52 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+
+  test('when blog list is empty', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list only has one blog post', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0,
+      },
+    ]
+
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('when list has multiple blog posts', () => {
+    const listWithMultipleBlogs = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: '63889dbc15e275b113ecbbbf',
+        title: 'Blog Post',
+        author: 'John Doe',
+        url: 'www.example.com/post1',
+        likes: 10,
+        __v: 0,
+      }
+    ]
+
+    const result = listHelper.totalLikes(listWithMultipleBlogs)
+    expect(result).toBe(15)
+  })
+
+})

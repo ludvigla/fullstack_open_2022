d1b3a82d331f25547ee9ec824e599fa74fd00719
ludvigla <ludvigla@kth.se>
2022-12-02 17:55:47 +0100

    Solution to exercise 4.15: bloglist expansion, step3

--- part4/blog-list-app/tests/blog_api.test.js
+++ part4/blog-list-app/tests/blog_api.test.js
@@ -2,9 +2,11 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
+const bcrypt = require('bcrypt')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -184,6 +186,40 @@ describe('blog api test', () => {
     expect(response.body).toHaveLength(helper.initialBlogs.length)
   })
 
+})
+
+// Users API tests
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', name: 'superuser', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'jdoe',
+      name: 'John Doe',
+      password: 'jdoe123',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
 })
 
 afterAll(() => {

54dfc8d1567d982d17273abf2ee5c2c91573bf76
ludvigla <ludvigla@kth.se>
2022-12-02 21:44:16 +0100

    Solution to exercise 4.19: bloglist expansion, step7

--- part4/blog-list-app/controllers/blogs.js
+++ part4/blog-list-app/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -16,10 +17,23 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
+// Ceate new blog posts with token authentication
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findOne({})
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = Blog({
     title: body.title,

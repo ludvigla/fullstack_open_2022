ceed6fe99c91f1bdc0439f9c1904171b3612b256
ludvigla <ludvigla@kth.se>
2022-12-27 12:50:36 +0100

    Solution to exercise 5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
     beforeEach(function() {
       cy.request('POST', 'http://localhost:3003/api/testing/reset')
+      const user = {
+        name: 'John Doe',
+        username: 'jdoe',
+        password: 'password'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', user) 
       cy.visit('http://localhost:3000')
     })
   
@@ -11,4 +17,25 @@ describe('Blog app', function() {
         cy.get('#login-form')
             .should('be.visible')
     })
+
+    describe('Login', function () {
+      it('user can log in with corect credentials', function () {
+        cy.contains('login').click()
+        cy.get('#username').type('jdoe')
+        cy.get('#password').type('password')
+        cy.get('#login-button').click()
+
+        cy.contains('John Doe logged in')
+      })
+
+      it('login fails with wrong credentials', function() {
+        cy.contains('login').click()
+        cy.get('#username').type('jdoe')
+        cy.get('#password').type('wrong')
+        cy.get('#login-button').click()
+
+        cy.get('.error').contains('Wrong user name or password')
+      })
+    })
+    
   })

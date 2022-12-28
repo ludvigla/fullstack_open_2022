197a35d3af0b7f39aa507904136dcd3e046616c1
ludvigla <ludvigla@kth.se>
2022-12-28 11:03:28 +0100

    Solution to exercise 6.1: unicafe revisited, step1

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,30 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  const copy = { ...state }
+  switch (action.type) {
+    case 'GOOD':
+      copy.good += 1
+      return copy
+    case 'OK':
+      copy.ok += 1
+      return copy
+    case 'BAD':
+      copy.bad += 1
+      return copy
+    case 'ZERO':
+      copy.good = 0
+      copy.ok = 0
+      copy.bad = 0
+      return copy
+    default: return state
+  }
+  
+}
+
+export default counterReducer
